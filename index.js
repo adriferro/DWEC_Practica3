1fdb125aaeb79dd0b19bf45bcaa39367f0f2c188
Adri <a.fernandezrodriguez2003@gmail.com>
2023-11-16 17:58:23 +0100

    Modificamos la funcion de la fecha

--- index.js
+++ index.js
@@ -1,13 +1,68 @@
-window.addEventListener("load",acceso);
+import { helloName } from "./hello.js";
+import { lengthName } from "./length.js";
+import { firstA } from "./first.js";
+import { lastA } from "./last.js";
+import { noFirstThree } from "./noFirstThree.js";
+import { capitalLetters } from "./capitalLetters.js";
+import { age } from "./age.js";
+import { season } from "./season.js";
+import { cosine } from "./cos.js";
+import { biggerNumber } from "./biggerNum.js";
+import { randomNumber } from "./random.js";
+
+export let name;
+export let surname;
+export let date; 
+
+window.addEventListener("load",logIn);
     
 function logIn(){
-    const allName = prompt("Introduzca su nombre completo")
-    const dayBorn = parseInt(prompt("Introduzca su DIA de nacimiento"))
-    const monthBorn = parseInt(prompt("Introduzca su MES de nacimiento"))
-    const ageBorn = parseInt(prompt("Introduzca su AÑO de nacimiento"))
-
 
     document.write("<h1 style='font-weight: bold;'>PRÁCTICA 3 - DWEC</h1>");
     document.write("<hr style='border: 1px solid black;' />");
     document.write(`<br>`)
+
+    function inputDate(){
+        let date1 = prompt("Introduzca su fecha de nacimiento en formato (dd/mm/yyyy):")
+
+        if(!date1){
+            alert("El campo no puede estar vacío.")
+            inputDate()
+        } else if(!/^\d{2}\/\d{2}\/\d{4}$/.test(date1)){
+            alert("Fecha en formato incorrecto.")
+            inputDate()
+        } else{
+            let partedDate = date1.split('/')
+            let day = parseInt(parts[0])
+            let month = parseInt(parts[1])
+            let year = parseInt(parts[2])
+
+
+            if(isNaN(day)||isNaN(month)||isNaN(year)||day===0||month===0){
+                alert("Fecha no válida.")
+            }else{
+                date= new Date(year, month-1, day)
+
+                if (isNaN(date.getTime())){
+                    alert("Fecha no válida.")
+                    inputDate()
+                }
+            }
+        }
+    }
+
+    func
 }
+
+
+helloName();
+lengthName();
+firstA();
+lastA();
+noFirstThree();
+capitalLetters();
+age();
+season();
+cosine();
+biggerNumber();
+randomNumber();
