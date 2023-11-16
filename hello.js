b8d85cd0a2c2da377d554f92a4d0e69ab239c97f
Adri <a.fernandezrodriguez2003@gmail.com>
2023-11-16 13:45:42 +0100

    Creamos las dos primeras funciones

---
+++ hello.js
@@ -0,0 +1,6 @@
+export function helloName(){
+    let primerNombre = nombreCompleto.split(" ")
+    document.write (`Buenos dias, <strong>${primerNombre[0]}</strong> `)
+    document.write(`<br>`)
+    document.write(`<br>`)
+}
---
+++ length.js
@@ -0,0 +1,5 @@
+export function lengthName(){
+    document.write(`Tu nombre completo (nombre y apellidos) tiene <strong>${nombreCompleto.length}</strong> caracteres, incluidos espacios`)
+    document.write(`<br>`)
+    document.write(`<br>`)
+}
